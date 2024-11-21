17b61dc5ebca90283b1adb11140b4b52b14e707d
Diego Jiménez <diego.o.jimz@gmail.com>
2024-11-21 11:19:37 -0600

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include "SpeedMonitor.h"
+int main(){
+SpeedMonitor instance {10};
+  instance.monitor();
+  instance.monitor();
+  instance.monitor();
+  instance.monitor();
+  instance.monitor();
+}
